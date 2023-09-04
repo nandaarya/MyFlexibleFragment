7a320f22c582bde8b650752a85a8e64ff117ea27
Nanda Arya Putra <nandaarya404@gmail.com>
2023-09-04 08:13:27 +0700

    add category fragment

--- app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
@@ -23,7 +23,15 @@ class HomeFragment : Fragment(), View.OnClickListener {
         btnCategory.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
